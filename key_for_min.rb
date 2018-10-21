3971b97e063e1b542364fa1e0b34fdb68ccfba17
Robert Chen <robertchen234@gmail.com>
2018-10-21 14:12:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest = name_hash.find(class = Integer)
+  name_hash.each do | item, price |
+    if price < lowest
+      lowest = item 
+    end 
+  end 
+  lowest
 end
