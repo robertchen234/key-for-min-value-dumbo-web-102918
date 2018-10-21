0e1d757542e423af700fba2c640b5d4f6993c58a
Robert Chen <robertchen234@gmail.com>
2018-10-21 15:54:43 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if key_for_min_value.empty?
+    nil 
+  else 
   max = 0
   name_hash.each do | item, price |
     if price > max
