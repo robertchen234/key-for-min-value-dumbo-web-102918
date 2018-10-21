3df4462c7c9ffe950ba564cb8ec135f01b7e5269
Robert Chen <robertchen234@gmail.com>
2018-10-21 15:58:50 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,18 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  if name_hash.empty?
+def key_for_min_value(hash)
+  if hash.empty?
     nil 
   else 
     max = 0
-    name_hash.map do | item, price |
+    hash.map do | item, price |
       if price > max
         max = price
       end 
     end 
     min = max
-    name_hash.map do | item, price |
+    hash.map do | item, price |
       if price < min
         min = item 
       end 
