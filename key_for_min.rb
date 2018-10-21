c80cc16123e228caa69a4df0870a9090f3379942
Robert Chen <robertchen234@gmail.com>
2018-10-21 16:07:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   key = nil
   min = nil
   hash.each do |item, price|
-     if min == nil or price < min
+     if min.nil? or price < min
        min = price
        key = item
      end
